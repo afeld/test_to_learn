41107888a03ff62ef5355e154da2950b1c249cdc
Aidan Feldman <aidan.feldman@gmail.com>
2013-06-20 04:43:41 -0400

    clear out todolist and test files

--- todo_list.rb
+++ todo_list.rb
@@ -1,37 +1,19 @@
-require 'yaml'
 require './todo'
 
 class TodoList
-  def initialize(filename)
-    @filename = filename
-    if File.exists?(filename)
-      serialized = File.read(filename)
-      @tasks = YAML.load(serialized)
-    else
-      @tasks = []
-    end
+  def initialize
+    # do stuff here
   end
 
   def add(title)
-    task = Todo.new(title)
-    @tasks << task
-    task
+    # do stuff here
   end
 
   def complete(index)
-    @tasks.delete_at(index)
+    # do stuff here
   end
 
   def to_s
-    output = ''
-    @tasks.each_with_index do |task, i|
-      output << "#{i}: #{task.title}\n"
-    end
-    output
-  end
-
-  def save
-    serialized = YAML.dump(@tasks)
-    File.open(@filename, 'w') {|f| f.write(serialized) }
+    # do stuff here
   end
 end
